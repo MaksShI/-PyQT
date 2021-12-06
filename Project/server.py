d424345bd21231dcc94edeb7e70d1f901bb24505
MaksShI <76102358+MaksShI@users.noreply.github.com>
2021-12-06 17:56:54 +0300

    Базы данных и PyQT
    Базы данных и PyQT

--- Project/server.py
+++ Project/server.py
@@ -1,12 +1,18 @@
 import argparse
 
+import mysql.connector
+from mysql.connector import Error
+from sqlalchemy.orm import sessionmaker
 import select
 import socket
 import sys
 import json
 import logging
+import pymysql
 import time
 
+from sqlalchemy import Column, create_engine, MetaData, Table, String, Integer, TupleType
+from sqlalchemy.orm import declarative_base
 import logs.server_log_config
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
     PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, MESSAGE, MESSAGE_TEXT, SENDER, DESTINATION, RESPONSE_400, EXIT
@@ -15,6 +21,72 @@ from decos import ServerDecorate
 
 SERVER_LOGGER = logging.getLogger('server')
 
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT, 
+  ip TEXT,
+  name TEXT NOT NULL, 
+  time TEXT, 
+  inf TEXT, 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+"""
+
+create_posts_table = """
+CREATE TABLE IF NOT EXISTS posts (
+  id INT AUTO_INCREMENT, 
+  title TEXT NOT NULL, 
+  description TEXT NOT NULL, 
+  user_id INTEGER NOT NULL, 
+  FOREIGN KEY fk_user_id (user_id) REFERENCES users(id), 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+"""
+
+create_database_query = "CREATE DATABASE sm_app"
+connection = create_connection("localhost", "root", "0246810m12357", "sm_app")
+
+execute_query(connection, create_users_table)
+execute_query(connection, create_posts_table)
+
+# create_database(connection, create_database_query)
 
 def process_message(message, names, listen_socks):
     if message[DESTINATION] in names and names[message[DESTINATION]] in listen_socks:
@@ -76,7 +148,7 @@ def main():
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transport.bind((listen_address, listen_port))
     transport.settimeout(0.5)
-
+    val = []
     clients = []
     messages = []
 
@@ -86,6 +158,8 @@ def main():
     while True:
         try:
             client, client_address = transport.accept()
+            val.append(client)
+            val.append(client_address)
         except OSError:
             pass
         else:
@@ -94,6 +168,7 @@ def main():
         recv_data_lst = []
         send_data_lst = []
         err_lst = []
+
         try:
             if clients:
                 recv_data_lst, send_data_lst, err_lst = select.select(clients, clients, [], 0)
@@ -104,15 +179,21 @@ def main():
                 try:
                     process_client_message(get_message(client_with_message), messages, client_with_message, clients,
                                            names)
-
                 except Exception:
                     clients.remove(client_with_message)
         for i in messages:
             try:
+
+                sql = "INSERT INTO users (ip, name, time, inf) VALUES ( %s, %s )"
+                val.append(i)
+
+                cursor = connection.cursor()
+                cursor.executemany(sql, val)
+                connection.commit()
                 process_message(i, names, send_data_lst)
             except Exception:
                 no_user_dict = RESPONSE_400
-                no_user_dict[ERROR] = f'Полюзователь {i[DESTINATION]} отключился от сервера'
+                no_user_dict[ERROR] = f'Пользователь {i[DESTINATION]} отключился от сервера'
                 send_message(names[i[SENDER]], no_user_dict)
                 del names[i[DESTINATION]]
         messages.clear()
